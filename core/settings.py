f85fd7f66790d87d7a6e1d652918debeb3085b87
pullpanther <softechstar.wlc@outlook.com>
2023-02-23 13:51:50 +0200

    email settings and smtp email-backend

--- core/settings.py
+++ core/settings.py
@@ -120,5 +120,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = '-'
+# EMAIL_HOST_PASSWORD = '-'
+# EMAIL_USE_TLS = True
+
+# LIST_OF_EMAIL_RECIPIENTS = ['-']
+# DEFAULT_FROM_EMAIL = '-'
+
 CELERY_BROKER_URL = "redis://redis:6379"
 CELERY_RESULT_BACKEND = "redis://redis:6379"
